2074958acd44f62b4123d2957a3fea42a3940ae8
jpablopachar <jpablopachar1993@gmail.com>
2025-04-17 01:45:33 -0500

    Actualiza la configuración de ESLint y Vite para incluir plugins y reglas adicionales

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,18 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import prettierConfig from 'eslint-config-prettier'
+import pluginImport from 'eslint-plugin-import'
+import pluginReact from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 
 export default [
   { ignores: ['dist'] },
   {
+    extends: [
+      js.configs.recommended,
+      pluginReact.configs.flat.recommended,
+    ],
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -17,11 +24,13 @@ export default [
       },
     },
     plugins: {
+      prettier: prettierConfig,
+      import: pluginImport,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...js.configs.recommended.rules,
+      ...prettierConfig.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
