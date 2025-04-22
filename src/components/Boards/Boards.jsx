d4d41736c30a383ada81e2f926b2672eeda82b4c
jpablopachar <jpablopachar1993@gmail.com>
2025-04-22 13:08:50 -0500

    feat(boards): implementar componente para mostrar tableros de usuario
    Se agrega un nuevo componente que utiliza React Query para obtener y
    mostrar los tableros de un usuario. Incluye manejo de estados de carga
    y error, así como un diseño para visualizar la información de cada
    tablero.

--- src/components/Boards/Boards.jsx
+++ src/components/Boards/Boards.jsx
@@ -1,7 +1,52 @@
-const Boards = () => {
-  return (
-    <div>Boards</div>
-  )
+import { useQuery } from '@tanstack/react-query'
+import { Link } from 'react-router'
+import { format } from 'timeago.js'
+import { getBoards } from '../../services/board.service'
+import Image from '../Image/Image'
+import './Boards.css'
+
+/**
+ * @typedef {Object} BoardProps
+ * @property {string} userId - ID del usuario al que pertenecen los tableros.
+ */
+
+/**
+ * Componente que muestra los tableros de un usuario en formato de colección.
+ * Utiliza React Query para obtener los datos de los tableros asociados al usuario.
+ *
+ * @component
+ * @param {BoardProps} props - Propiedades del componente.
+ * @returns {JSX.Element} Listado de tableros del usuario con vista previa de imagen y detalles.
+ */
+const Boards = ({ userId }) => {
+	const { isPending, error, data } = useQuery({
+		queryKey: ['boards', userId],
+		queryFn: async () => await getBoards(userId),
+	})
+
+	if (isPending) return 'Loading...'
+
+	if (error) return `An error has occurred: ${error.message}`
+
+	return (
+		<div className="collections">
+			{data?.map((board) => (
+				<Link
+					to={`/search?boardId=${board._id}`}
+					className="collection"
+					key={board._id}
+				>
+					<Image path={board.firstPin.media} alt="" />
+					<div className="collectionInfo">
+						<h1>{board.title}</h1>
+						<span>
+							{board.pinCount} Pins · {format(board.createdAt)}
+						</span>
+					</div>
+				</Link>
+			))}
+		</div>
+	)
 }
 
 export default Boards
