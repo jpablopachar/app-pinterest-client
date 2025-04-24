e8ce4c6691015d9bf211bd445b970946bb79527e
jpablopachar <jpablopachar1993@gmail.com>
2025-04-23 19:06:16 -0500

    feat(routes): agregar rutas para páginas de detalle y búsqueda de pines
    Se han añadido rutas para la página de detalle de pines individuales y
    la página de búsqueda. Esto mejora la navegación y la accesibilidad
    de las funcionalidades relacionadas con los pines.

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,25 +1,31 @@
 import { Suspense, lazy } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from './layouts/Main/Main'
+import PostPage from './pages/PostPage/PostPage'
+import ProfilePage from './pages/ProfilePage/ProfilePage'
+import SearchPage from './pages/SearchPage/SearchPage'
 
 const AppRoutes = () => {
-  const Home = lazy(() => import('./pages/Home/Home'))
-  const CreatePage = lazy(() => import('./pages/CreatePage/CreatePage'))
-  const Auth = lazy(() => import('./pages/Auth/Auth'))
+	const Home = lazy(() => import('./pages/Home/Home'))
+	const CreatePage = lazy(() => import('./pages/CreatePage/CreatePage'))
+	const Auth = lazy(() => import('./pages/Auth/Auth'))
 
-  return (
-    <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route element={<Main />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/create" element={<CreatePage />} />
-          </Route>
-          <Route path="/auth" element={<Auth />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route element={<Main />}>
+						<Route path="/" element={<Home />} />
+						<Route path="/create" element={<CreatePage />} />
+						<Route path="/pin/:id" element={<PostPage />} />
+						<Route path="/:username" element={<ProfilePage />} />
+						<Route path="/search" element={<SearchPage />} />
+					</Route>
+					<Route path="/auth" element={<Auth />} />
+				</Routes>
+			</Suspense>
+		</BrowserRouter>
+	)
 }
 
 export default AppRoutes
